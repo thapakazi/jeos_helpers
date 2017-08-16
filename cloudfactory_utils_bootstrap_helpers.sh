5210cd56af64a988234872ebf6531aa7c9153216
Milan Thapa <milans.thapa78@gmail.com>
2017-08-16 15:00:22 +0545

    adding all helper functions in gist

--- cloudfactory_utils_bootstrap_helpers.sh
+++ cloudfactory_utils_bootstrap_helpers.sh
@@ -19,3 +19,59 @@ utils_pull_private_key (){
     echo "#SECURITY :: PLEASE CLEAN UP THE FILE $PRIVATE_KEY_IN_LOCAL"
     echo "#SECURITY :: rm -rf $PRIVATE_KEY_IN_LOCAL"
 }
+
+
+utils_install_dependent_galaxy_roles(){
+    # we need to install these roles 
+    DEPENDENT_ROLES=( williamyeh.prometheus franklinkim.newrelic )
+    ansible-galaxy install "${DEPENDENT_ROLES[@]}"
+}
+
+# override 
+bootstrap(){
+    BOOTSTRAP_BRANCH="${SCALE_BRANCH:-master}"
+    BOOTSTRAP_PLAYBOOK="${BOOTSTRAP_PLAYBOOK:-devel.yml}"
+    VAULT_PASS_FILE="$HOME/.vault_pass" # this is supposed to be present in JeOS
+
+    BOOTSTRAP_GITHUB_URL="github.com:cloudfactory/scale"
+    BOOTSTRAP_TMP_PULL_DIR="$USERDATA_TMPDIR/scale"
+
+    [ -z "$SKIP_BOOTSTRAP" ] \
+	&& ansible-pull -C $BOOTSTRAP_BRANCH \
+			--full -d ${BOOTSTRAP_TMP_PULL_DIR} \
+			-i 'localhost' -U git@${BOOTSTRAP_GITHUB_URL}.git \
+			--accept-host-key $BOOTSTRAP_PLAYBOOK -vvvv \
+			--vault-password-file=${VAULT_PASS_FILE} # ||  curl http://169.254.169.254/latest/user-data | bash -xv
+}
+
+deployment(){
+
+    # get the keys first
+    su - deploy -c 'utils_pull_private_key '
+
+    #safely assuming, bootstrap layers above successfully completed.
+    source /etc/profile.d/cloudfactory_utils*
+
+    # get the tag: project and run play accordingly
+    DEPLOYMENT_TMP_PULL_DIR="/tmp/ops-automata" && rm -rf $DEPLOYMENT_TMP_PULL_DIR
+    DEPLOYMENT_GITHUB_URL="github.com:cloudfactory/ops-automata"
+    PROJECT_TO_DEPLOY=$(cloudfactory_get_value_for_tag project) #clientplatform
+
+    DEPLOYMENT_PLAYBOOK="${PROJECT_TO_DEPLOY}.yml"
+    EC2SPIN_ROLE=${EC2SPIN_ROLE:-worker}
+    SKIP_TAGS=${DEPLOMENT_SKIP_TAGS:-"ec2spin,ansicap,runit_unicorn"}
+
+    DEBUG_FLAG=${DEBUG_FLAG:-'-vvvv'}
+    ansible-pull -C $PROJECT_TO_DEPLOY --full -d ${DEPLOYMENT_TMP_PULL_DIR} \
+		 -i local.ini -U git@${DEPLOYMENT_GITHUB_URL}.git \
+		 --accept-host-key $DEPLOYMENT_PLAYBOOK  \
+		 --skip-tags=${SKIP_TAGS} -e EC2SPIN_ROLE=${EC2SPIN_ROLE} ${DEBUG_FLAG}
+}
+
+post_cleanup(){
+    echo "Post cleanup actions: removing the ${PRIVATE_KEY_IN_LOCAL}"
+    # improve it later, i know hardcoding sucks
+    rm -rf ~/.ssh/id_rsa ~deploy/.ssh/id_rsa
+    
+    # rm /var/lib/cloud/instance/{sem/config_scripts_user,boot-finished}
+}
